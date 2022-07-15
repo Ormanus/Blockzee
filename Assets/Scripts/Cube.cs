cd5bf8c2bd23181a02c6a567d2212bfd9a27232c
Ors <38985578+Riku-S@users.noreply.github.com>
2022-07-15 23:10:04 +0300

    Animate cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    public enum CubeState
+    {
+        Idle,
+        Moving
+    }
+
+    [HideInInspector]
+    public CubeState State { get; private set; }
+
     public Color color;
     public int diceId;
+    public const float animationDuration = 0.2f;
+
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+
+    Vector3 targetPosition;
+    Quaternion targetRotation;
+
+    float animationStartTime;
+
+    public Cube()
+    {
+        State = CubeState.Idle;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,8 +57,63 @@ public class Cube : MonoBehaviour
         }
         return eyes; 
     }
-    internal void Move(CubeController.Direction right)
+
+    void MoveCube()
     {
-        throw new NotImplementedException();
+        if (State == CubeState.Moving)
+        {
+            float phase = (Time.time - animationStartTime) / animationDuration;
+            if (phase >= 1)
+                State = CubeState.Idle;
+
+            phase = Math.Min(phase, 1);
+
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, phase) + Vector3.up * 0.33f * Mathf.Sin(phase * Mathf.PI);
+            transform.rotation = Quaternion.Lerp(originalRotation, targetRotation, phase);
+        }
+
+    }
+
+    private void Update()
+    {
+        MoveCube();
+    }
+
+    internal void Move(CubeController.Direction direction)
+    {
+        if (State != CubeState.Idle)
+            return;
+
+        State = CubeState.Moving;
+
+        animationStartTime = Time.time;
+
+        float amount = 90;
+        Vector3 axis = Vector3.right;
+
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+
+        switch (direction)
+        {
+            case CubeController.Direction.Up:
+                targetPosition = originalPosition + Vector3.forward;
+                break;
+            case CubeController.Direction.Down:
+                targetPosition = originalPosition + Vector3.back;
+                amount = -amount;
+                break;
+            case CubeController.Direction.Left:
+                targetPosition = originalPosition + Vector3.left;
+                axis = Vector3.forward;
+                break;
+            case CubeController.Direction.Right:
+                targetPosition = originalPosition + Vector3.right;
+                axis = Vector3.forward;
+                amount = -amount;
+                break;
+        }
+
+        targetRotation =  Quaternion.Euler(axis * amount) * transform.rotation;
     }
 }
